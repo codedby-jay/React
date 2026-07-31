ef940cb13baf01ca45334da3ef5b11c04119bf01
Jay Prajapati <jayprajapati98006@gmail.com>
2026-07-31 16:28:54 +0530

    innitial commit

---
+++ FirstProject/src/About.jsx
@@ -0,0 +1,8 @@
+let About = (props)=>{
+
+  console.log(props);
+  
+  return <h1>This is About page</h1>;
+}
+
+export default About;
---
+++ FirstProject/src/App.jsx
@@ -0,0 +1,14 @@
+import About from "./About";
+
+
+
+let App = ()=>{
+  return <div>
+    <h1>Hello</h1>
+    <h3>How Are you?</h3>
+    <h6>Bye</h6>
+    <About width="300" age={20} height="200"/>
+  </div>;
+}
+
+export default App; 
